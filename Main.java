311aa1db8fe6bc0b0c25dff678c0240e167743c6
Muazzez Erdem <115171059+stryvinx@users.noreply.github.com>
2023-11-28 20:26:12 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package deneme_yaz_gel_mavensiz_package;
+
+import org.apache.common.lang3.StringUtils;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String metin = "Me di po l";
+		
+		System.out.println("Merhaba!");
+
+	}
+
+}
